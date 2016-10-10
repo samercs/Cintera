36ad1f7601b344147901535ce1cac2b2cf23d7cf
samer <samer_mail_2006@yahoo.com>
2016-10-10 13:48:20 +0300

    test view1 if work

--- Src/Cintera.Web/Controllers/HomeController.cs
+++ Src/Cintera.Web/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cintera.DAL;
 
 namespace Cintera.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private CenteraContext _centera;
+
+        public HomeController()
+        {
+            _centera = new CenteraContext();
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+            var model = _centera.View1.ToList();
+            return View(model);
         }
 
         public ActionResult About()
